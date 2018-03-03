ac0700181efbc18dfa2550cf31a1ab7798ab18df
danlitwin <danlitwin@gmail.com>
2018-03-03 23:14:09 +0000

    Overhaul app reloading and key structure

--- todoist-trello.py
+++ todoist-trello.py
@@ -4,32 +4,26 @@ import requests
 import pickle
 from todoist.api    import TodoistAPI
 from trello         import TrelloClient
+import api_keys
 
 td_projects_to_sync = ['Home']
 
-api_keys = {
-'todoist': {
-    'key':   '18c31701631652e1d742f5ba2de7ad8603ebee6b'},
-'trello': {
-    'key':   'c44c1ecbbc116c60827000c419e5cb1d',
-    'token': 'aafd625924a8a03cc5028894cd59cf836fa23352416f66a3f9f806b9cc783e23'}
-}
 
 def main():
     trello_test()
 
 
-class TodoistTrelloSync
+# class TodoistTrelloSync
 
 
 
 def todoist_test():
     td = TodoistAPI(api_keys['todoist']['key'])
     td.sync()
-    for project in td.projects:
-        if sync_td_projects.count(project.name) = 0:
-    ':'.join('Home:Office:Molding'.split(':')[:-1])
-    plist = [[p['name'], p['item_order'], p['indent'], p['id']] for p in td['projects']]
+    # for project in td.projects:
+    #     if sync_td_projects.count(project.name) = 0:
+    #         ':'.join('Home:Office:Molding'.split(':')[:-1])
+    # plist = [[p['name'], p['item_order'], p['indent'], p['id']] for p in td['projects']]
 
 
 def trello_test():
@@ -38,7 +32,6 @@ def trello_test():
         token =         api_keys['trello']['token']
     )
     all_boards = tr.list_boards()
-
     url = 'https://api.trello.com/1/members/me/boards'
     response = requests.request('GET', url, params=api_keys['trello'])
     print(response.text)
