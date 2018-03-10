d491d1e419d253a463d5a8850dd626e735d3101d
danlitwin <danlitwin@users.noreply.github.com>
2018-03-10 12:51:17 -0500

    pycodestyle corrections

--- callback.py
+++ callback.py
@@ -9,18 +9,20 @@ from box import SBox
 from _app_keys import api_keys
 API = SBox(api_keys, frozen_box=True, box_it_up=True)
 
+
 @route('/services')
 @route('/services/')
 def hello_world():
     return 'Hello from Bottle service! Update works, test #4'
 
 
-def check_auth(req=None,abort_if_false=True):
+def check_auth(req=None, abort_if_false=True):
     if req is None:
         req = request
-    auth_valid = req.headers.get('Authorization')==API.master.token
+    auth_valid = req.headers.get('Authorization') == API.master.token
     if not auth_valid and abort_if_false:
         abort(401, 'Invalid authorization')
     return auth_valid
+
 
 bottle_app = default_app()
