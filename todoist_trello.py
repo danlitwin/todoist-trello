58d0af95ae2b0e13078938accf5f6b40f9430487
danlitwin <danlitwin@users.noreply.github.com>
2018-03-11 00:27:49 -0500

    Added TD functions

--- todoist_trello.py
+++ todoist_trello.py
@@ -1,19 +1,21 @@
 import requests
-# import uuid
+import uuid
 # import json
 from keybox import keybox
 # from todoist.api    import TodoistAPI
 # from trello         import TrelloClient
 
+# class TodoistREST():
+
 
 def td_get_labels():
-    labels = td_rest(endpoint='labels', method='GET').json()
+    labels = td_request(endpoint='labels', method='GET').json()
     return {k['name']: k['id'] for k in labels}
 
 
 def td_get_tasks(project=None, label=None):
     params = {'project_id': project, 'label_id': label}
-    tasks = td_rest(endpoint='tasks', method='GET', params=params)
+    tasks = td_request(endpoint='tasks', method='GET', params=params)
     return tasks.json()
 
 
@@ -23,22 +25,21 @@ def td_find_task(name, prefix=True, cache_tasks=None):
     return matches[0]
 
 
-def td_rest(endpoint='', method='POST', **kwargs):
+def td_request(endpoint='', method='POST', **kwargs):
     url = 'https://beta.todoist.com/API/v8/' + endpoint.strip('/')
     kwargs.setdefault('headers', {}).update({
         'Authorization': 'Bearer {}'.format(keybox.todoist.key)
-        # , 'Content-Type':  'application/json'
-        # , 'X-Request-Id':  str(uuid.uuid4())
-    })
-    response = requests.request(method, url, **kwargs)
-    return response
+    }).update({
+        'Content-Type':  'application/json',
+        'X-Request-Id':  str(uuid.uuid4())
+    } if method == 'POST' or 'data' in kwargs or 'json' in kwargs else {})
+    return requests.request(method, url, **kwargs)
 
 
 def trello_request(method='POST', endpoint='', **kwargs):
     url = 'https://api.trello.com/1/' + endpoint.strip('/') + '/'
     kwargs.setdefault('query', {}).update(keybox.trello.dict)
-    response = requests.request(method, url, **kwargs)
-    return response
+    return requests.request(method, url, **kwargs)
 
 
 # ==============================================================================
